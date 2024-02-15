9f1cf13d3beaf2432e2af33d9e20aead0e107c17
ElJayRight <liamwri2003@hotmail.com>
2024-02-15 22:28:20 +1100

    added backend sql code

---
+++ sql_api.py
@@ -0,0 +1,57 @@
+import sqlite3
+conn = sqlite3.connect('data.db')
+
+def is_in_db(method, language=None):
+    cursor = conn.cursor()
+   
+    if language:
+        cursor.execute("select * from methods where method_name = ? and language = ?", (method,language))
+    else:
+        cursor.execute("select * from methods where method_name = ?", (method, ))
+
+    result = cursor.fetchone()
+    cursor.close()
+    if result:
+        return True
+    else:
+        return False
+
+def add_record(method, language, code_file):
+    if is_in_db(method, language):
+        return
+    cursor = conn.cursor()
+    cursor.execute('''INSERT INTO methods (method_name, language, code)
+                   VALUES (?, ?, ?)''', (method, language, code_file))
+    conn.commit()
+    cursor.close()
+
+def get_data(method, language=None):
+    cursor = conn.cursor()
+    if not is_in_db(method, language):
+        return None
+    if language:
+        cursor.execute("select code from methods where method_name = ? and language = ?", (method, language))
+        result = cursor.fetchone()
+        result = result[0]
+    else:
+        cursor.execute("select language from methods where method_name = ?", (method, ))
+        result = cursor.fetchall()
+        result = [i[0] for i in result]
+    if result:
+        return result
+    return False 
+
+def build():
+    cursor = conn.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS methods (
+                   id INTEGER PRIMARY KEY,
+                   method_name TEXT NOT NULL,
+                   language TEXT NOT NULL,
+                   code TEXT NOT NULL
+                )''')
+
+    conn.commit()
+
+if __name__ == "__main__":
+    build()
